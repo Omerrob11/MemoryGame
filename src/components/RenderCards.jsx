49bff2408f8349069bdc41a69949c8481253f8b6
Omer Miloh <omerrob11@gmail.com>
2024-10-06 15:56:49 +0300

    feat: add array shuffling algorithm in O(n)

--- src/components/RenderCards.jsx
+++ src/components/RenderCards.jsx
@@ -1,11 +1,20 @@
 import "../styles/cards.css";
 import { useState } from "react";
+const numOfRenderedPokemon = 9;
 
 function RenderCards({ pokemonList, handleCardClick }) {
+  // we only want to render around 6-7 of the array.
+  // so let's create 2 arrays
+  // all list of divs
+  // and a rendered list that we will actually render.
+  // each time, we only pick a couple of those.
+
+  const renderedPokemonList = getRandomItems(pokemonList, numOfRenderedPokemon);
+  console.log(renderedPokemonList);
   return (
     <div className="cards-container">
-      {pokemonList.length > 0 &&
-        pokemonList.map((pokemon) => {
+      {renderedPokemonList.length > 0 &&
+        renderedPokemonList.map((pokemon) => {
           return (
             <div
               className="card-single"
@@ -24,3 +33,17 @@ function RenderCards({ pokemonList, handleCardClick }) {
 }
 
 export default RenderCards;
+
+// shuffle the array using known algorithm run in O(n)
+function getRandomItems(array, count) {
+  const shuffled = [...array];
+
+  for (let i = shuffled.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    const temp = shuffled[i];
+    shuffled[i] = shuffled[j];
+    shuffled[j] = temp;
+  }
+
+  return shuffled.slice(0, count);
+}
